c237f9326edbcaa5a4ed8b0f7ebae974de0cd857
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-12-15 20:14:13 +0600

    min w added in service card

--- asset/js/resources.js
+++ asset/js/resources.js
@@ -62,18 +62,20 @@ cards.innerHTML = resources
     (card) =>
       `
    	<div class='lg:h-[157.5px]'>
-			<div class=" rounded-lg border bg-white  hover:bg-gray-100 dark:bg-primary dark:border-gray-700 dark:hover:bg-gray-800 cursor-pointer p-4 shadow-sm transition hover:shadow-lg sm:p-5">
+			<a href="#" class="block rounded-lg border bg-white  hover:bg-gray-100 dark:bg-primary dark:border-gray-700 dark:hover:bg-gray-800 cursor-pointer p-4 shadow-sm transition hover:shadow-lg sm:p-5">
          	<h3 class="mt-0.5 lg:min-h-[28px] text-lg font-inter font-medium text-slate-800 dark:text-slate-100">
            	 	${card.title}
          	</h3>
          	<p class="mt-2 text-sm lg:min-h-[45.5px] leading-relaxed text-gray-500 dark:text-gray-400">
             	${card.quote}
-        		 </p>
-				<a href='/' class='mt-2  lg:min-h-[24px] text-blue-700 font-bold tracking-wider inline-flex items-center'>
-					<span class='inline-flex items-center  space-x-2'>Read more</span>
-					<span class='ml-2'><i class="fa-solid fa-link"></i></span>
-				</a>
-    		</div>
+        	</p>
+				  <div>
+            <a href='/' class='mt-2  lg:min-h-[24px] text-blue-700 font-bold tracking-wider inline-flex items-center'>
+					    <span class='inline-flex items-center  space-x-2'>Read more</span>
+					    <span class='ml-2'><i class="fa-solid fa-link"></i></span>
+				    </a>
+          </div>
+    		</a>
 		</div>
 	`
   )
